36a61b320c2b570bd8e3acc80c1516498d49de75
Katherine Trammell <ktrammell@gmail.com>
2015-03-17 13:42:29 -0400

    started basic views

---
+++ jsx/react_views.jsx
@@ -0,0 +1,18 @@
+kt.views = {};
+
+(function(views){
+
+  var PageOne = React.createClass({
+    render: function() {
+      return <div>Page One</div>
+    }
+  });
+
+  var PageTwo = React.createClass({
+    render: function() {
+      return <div>Page Two</div>
+    }
+  });
+
+  
+};
